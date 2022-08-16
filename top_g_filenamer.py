a7985d6cd8a037bb3bc9094bc1a19b13ed0ae90e
KurtReti <kurt.reti@gmail.com>
2022-08-17 00:39:56 +1000

    user input added and functionality to remove undersores

--- top_g_filenamer.py
+++ top_g_filenamer.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python
 import os
 
-list_of_files = os.listdir()
-for i in list_of_files:
-    if i.endswith('.pdf'):
-        newname = i.replace(" ","_")
-        os.rename(i, newname)
-print(i)
+print("Add or remove underscores from file names")
+file_extension = input("What type of files do you want to replace? Enter file extension:")
+
+for i in range(0,5):
+
+    print('Enter 1 to add underscores.\n')
+
+    print('Enter 2 to remove underscores.\n')
+
+
+    choice = int(input('Enter your choice:'))
+
+    if (choice == 1):
+
+        list_of_files = os.listdir()
+        for i in list_of_files:
+                if i.endswith(file_extension):
+                    newname = i.replace(" ","_")
+                    os.rename(i, newname)
+        print(i)
+
+    elif (choice == 2):
+
+        list_of_files = os.listdir()
+        for i in list_of_files:
+                if i.endswith(file_extension):
+                    newname = i.replace("_"," ")
+                    os.rename(i, newname)
+        print(i)
+
+
+    else:
+        print('Invalid choice')
